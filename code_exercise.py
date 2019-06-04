ab25a554c6d01d074fdc233389a556af53cca8de
CTSDH <luctsdh@luc.edu>
2019-06-04 15:08:31 -0400

    changed a bunch of stuff

---
+++ code_exercise.py
@@ -0,0 +1,10 @@
+address = {
+"name": "Liz Hopwood",
+"street": "1344 1/2 W. Argyle St.",
+"city": "Chicago",
+"state": "Illinois",
+"zip": "60640"
+}
+print(address['name'])
+print(address['street'])
+print(address['city'] + ', '  + address['state'] +  address['zip'])
