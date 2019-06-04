4fd9b1dcbf7d6269e8e2fc52308fec9ba6802fb6
CTSDH <luctsdh@luc.edu>
2019-06-04 16:32:40 -0400

    added file

--- for_loops1.py
+++ for_loops1.py
@@ -1,6 +1,24 @@
-numbers = [20, 40, 60, 80, 100]
-results = []
+# numbers = [20, 40, 60, 80, 100]
+# results = []
+#
+# for number in numbers:
+#         results.append(number - 10)
+# print(results)
 
-for number in numbers:
-        results.append(number - 10)
-print(results)
+# make a new list places_to_visit
+#places_to_visit = ['Portugal', 'Thailand', 'Norway', 'Ireland']
+
+# loop over that list and call each element place
+# for place in places_to_visit:
+#     # print out the place sentence
+#         print("I would like to visit " + place + '.')
+
+# fruits = ['kiwi', 'strawberry', 'plum']
+# for fruit in fruits:
+#         if fruit =="plum":
+#                 print(fruit)
+
+hilt_class = ['Liz', 'Lorena', 'Jennifer', 'Kenna']
+for name in hilt_class:
+            if name =="Liz":
+                print('My name is ' + name)
